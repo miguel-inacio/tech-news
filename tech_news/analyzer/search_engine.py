3668f0bee80d7cef7e1a48fd041ee0ac19631086
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-14 11:02:25 -0300

    feat: adiciona buscar por título de notícia

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,11 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title: str) -> list[tuple]:
+    query = {"title": {"$regex": title, "$options": "i"}}
+    matching_news = search_news(query)
+    return [(news["title"], news["url"]) for news in matching_news]
 
 
 # Requisito 8
