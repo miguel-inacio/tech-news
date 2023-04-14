697691d817c390144d3e9fd3e81007e27e176c43
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-14 11:33:26 -0300

    feat: adiciona busca por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -9,8 +10,15 @@ def search_by_title(title: str) -> list[tuple]:
 
 
 # Requisito 8
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date: str) -> list[tuple]:
+    try:
+        fixed_date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        query = {"timestamp": fixed_date}
+        matching_dates = search_news(query)
+        return [(news["title"], news["url"]) for news in matching_dates]
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
