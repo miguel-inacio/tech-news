563f855e0d6c427c9e3b30593cd0bcbaa1506cd1
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-04-14 10:27:07 -0300

    feat: adiciona mock de notícias

---
+++ tests/reading_plan/mocked_news.py
@@ -0,0 +1,22 @@
+mocked_news = [
+    {
+        "title": "Título 1",
+        "reading_time": 5,
+    },
+    {
+        "title": "Título 2",
+        "reading_time": 2,
+    },
+    {
+        "title": "Título 3",
+        "reading_time": 1,
+    },
+    {
+        "title": "Título 4",
+        "reading_time": 50,
+    },
+    {
+        "title": "Título 5",
+        "reading_time": 7,
+    },
+]
